a6abd7603b1217c2c52cfe2f36727bcc88162304
Eben-Success <aebenezer237@gmail.com>
2022-03-15 19:16:53 +0000

    Modified

---
+++ Your Life In Weeks.py
@@ -0,0 +1,18 @@
+# The program requests for user's current age and
+# calculates the number of x days, y weeks, and z months left.
+
+# 365 days in a year
+# 52 weeks in a year
+# 12 months in a year
+
+# This program assumes user dies at the age of 90 years
+
+const_age = 90
+
+current_age = input("What is your current age in years?\n")
+
+remaining_years = const_age - int(current_age)
+
+
+
+print(f"You have")
