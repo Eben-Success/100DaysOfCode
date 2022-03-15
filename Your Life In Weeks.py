9fab38c68f39deb70cc84f651e768c2385e90c28
Eben-Success <aebenezer237@gmail.com>
2022-03-15 19:19:36 +0000

    Modified

--- Your Life In Weeks.py
+++ Your Life In Weeks.py
@@ -13,6 +13,6 @@ current_age = input("What is your current age in years?\n")
 
 remaining_years = const_age - int(current_age)
 
+remaining_years = remaining_years * 365
 
-
-print(f"You have")
+print(f"You have {remaining_years}")
