9c0f0f0095b60e935f45826e664c40cf70f88bad
Eben-Success <71809927+Eben-Success@users.noreply.github.com>
2022-07-06 11:05:13 +0000

    right & left choice

--- Day 3/TreasureIsland.py
+++ Day 3/TreasureIsland.py
@@ -39,3 +39,14 @@ _______________________$$$$$________|\\\\\\\_________________
 print("Welcome to Teasure Island.")
 print("Your mission is to find the treasure.")
 
+choice1 = input('You\'re at the crossroad, where do you want to go? Type "left" or "right".').lower()
+
+if choice1 == 'left':
+    # Continue the game
+    input("You've come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swin" to swim across")
+
+else:
+    print("You fell into a hole. Game over")
+
+
+
