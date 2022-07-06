47b79646e0d71350c76751dd289f1229d0f73f7a
Eben-Success <71809927+Eben-Success@users.noreply.github.com>
2022-07-06 09:51:09 +0000

    underweight

--- Day 3/BMI 2.0.py
+++ Day 3/BMI 2.0.py
@@ -1,5 +1,8 @@
 height = float(input("Enter your height in m: "))
 weight = float(input("Enter your weight in kg: "))
 
-bmi = weight / height ** 2
+bmi = round(weight / height ** 2)
+
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, you are underweight.")
 
