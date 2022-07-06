939b10effcc80bfcec76e68cfe0388ca5730dde9
Eben-Success <71809927+Eben-Success@users.noreply.github.com>
2022-07-06 11:19:28 +0000

    else added

--- Day 3/TreasureIsland.py
+++ Day 3/TreasureIsland.py
@@ -39,11 +39,11 @@ _______________________$$$$$________|\\\\\\\_________________
 print("Welcome to Teasure Island.")
 print("Your mission is to find the treasure.")
 
-choice1 = input('You\'re at the crossroad, where do you want to go? Type "left" or "right".').lower()
+choice1 = input('You\'re at the crossroad, where do you want to go? Type "left" or "right".\n').lower()
 
 if choice1 == 'left':
     # Continue the game
-    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swin" to swim across. ').lower()
+    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swin" to swim across. \n').lower()
 
     if choice2 == 'wait':
         # Game will continue
@@ -51,7 +51,7 @@ if choice1 == 'left':
         choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color "
               "do you choose?\n").lower()
         if choice3 == 'red':
-            print("It's a room full of fire. Game Over.")
+            print("It's a room full of fire. Game Over.\n")
         elif choice3 == 'yellow':
             print("You found the treasure1 You Win!")
         elif choice3 == 'blue':
