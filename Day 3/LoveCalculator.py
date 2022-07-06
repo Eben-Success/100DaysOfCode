f08fd5256c12bafd1483d22df683fce185b0186b
Eben-Success <71809927+Eben-Success@users.noreply.github.com>
2022-07-06 00:31:03 +0000

    true

--- Day 3/LoveCalculator.py
+++ Day 3/LoveCalculator.py
@@ -1,3 +1,23 @@
 
 # This program test the compatibility between two people.
 
+print("Welcome to the Love Calculator\n")
+
+name1 = input("What is your name?: \n")
+name2 = input("What is their name?: \n")
+
+combined_string = name1 + name2
+lowwer_case_string = combined_string.lower()
+
+t = lowwer_case_string.count("t")
+r = lowwer_case_string.count("r")
+u = lowwer_case_string.count("u")
+e = lowwer_case_string.count("e")
+
+true = t + r + u + e
+print("Total is: " + true)
+
+t = lowwer_case_string.count("t")
+r = lowwer_case_string.count("r")
+u = lowwer_case_string.count("u")
+e = lowwer_case_string.count("e")
