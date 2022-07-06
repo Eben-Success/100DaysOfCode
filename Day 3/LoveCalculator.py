753a375e11fb8163377e9bd3ecc8b3f70a159d72
Eben-Success <71809927+Eben-Success@users.noreply.github.com>
2022-07-06 00:39:05 +0000

    done with the coding

--- Day 3/LoveCalculator.py
+++ Day 3/LoveCalculator.py
@@ -27,3 +27,12 @@ print("Total is: " + str(love))
 
 love_score = str(true) + str(love)
 print("Your score is: " + love_score)
+
+if (love_score) < 10 or (love_score > 90):
+    print(f"Your love score is {love_score}, you go together like coke and mentos")
+
+elif (love_score >= 40) and (love_score <= 50):
+    print(f"Your score is {love_score}, you are alright together")
+
+else:
+    print(f"Your score is {love_score}.")
