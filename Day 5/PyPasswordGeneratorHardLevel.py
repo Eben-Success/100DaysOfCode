b32424fcee2b3b7441ace809804b9bd28fd79024
Eben-Success <aebenezer237@gmail.com>
2022-07-11 23:19:39 +0000

    readme on project for day 5

--- Day 5/PyPasswordGeneratorHardLevel.py
+++ Day 5/PyPasswordGeneratorHardLevel.py
@@ -12,9 +12,9 @@ nr_letters = int(input("How many letters would you like in your password\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Easy Level
+# Hard Level
 password = ""
-#nr_letters = 4
+
 for char in range(1, nr_letters + 1):
     password += random.choice(letters)
 
