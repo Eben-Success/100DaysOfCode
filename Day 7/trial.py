ac91fc42a7b5c1f99997949c71d44e306f2c1ace
Eben-Success <aebenezer237@gmail.com>
2022-07-19 12:57:22 +0000

    update hangman.py

--- Day 7/trial.py
+++ Day 7/trial.py
@@ -1,28 +1,28 @@
-import random
-
-word_list = ["realmadrid", "barcelona", "chelsea", "arsenal"]
-
-chosen_word = random.choice(word_list)
-
-# Test the code by checking the random number
-print(f'\n Ops, the right word is {chosen_word} \n')
-
-word_length = len(chosen_word)
-
-# Display dashes equal to the random letter the user
-# has to guess
-
-display = []
-for _ in range(word_length):
-    display += "_"
-print(display)
-
-# Request for a letter from user
-guess = input("Guess a number").lower()
-
-for position in range(word_length):
-    letter = chosen_word[position]
-
-    if letter == guess:
-        display[position] = letter
-print(display)
+# import random
+#
+# word_list = ["realmadrid", "barcelona", "chelsea", "arsenal"]
+#
+# chosen_word = random.choice(word_list)
+#
+# # Test the code by checking the random number
+# print(f'\n Ops, the right word is {chosen_word} \n')
+#
+# word_length = len(chosen_word)
+#
+# # Display dashes equal to the random letter the user
+# # has to guess
+#
+# display = []
+# for _ in range(word_length):
+#     display += "_"
+# print(display)
+#
+# # Request for a letter from user
+# guess = input("Guess a number").lower()
+#
+# for position in range(word_length):
+#     letter = chosen_word[position]
+#
+#     if letter == guess:
+#         display[position] = letter
+# print(display)
