a200b87c0b3975695cfac4ae25804063d21482fc
Eben-Success <aebenezer237@gmail.com>
2022-07-19 01:56:55 +0000

    update day 7

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -6,19 +6,19 @@ chosen_word = random.choice(word_list)
 
 print(f'Pssst, the solution is {chosen_word}')
 
-display = []
-for letter in chosen_word:
-    display += "_"
-print(display)
+# display = []
+# for letter in chosen_word:
+#     display += "_"
+# print(display)
 
 # Alternative approach
 
-len_chosen_word = len(chosen_word)
-
-for i in range(0, len_chosen_word + 1):
-    print()
-
+display = []
+for _ in range(len(chosen_word)):
+    display += "_"
+print(display)
 guess = input("Guess a letter: \n").lower()
+
 
 # for letter in chosen_word:
 #     if letter == guess:
