03d56553454e7ff7fbfd6f9d134ecf518365b958
Eben-Success <aebenezer237@gmail.com>
2022-07-19 10:12:01 +0000

    update hangman.py

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -13,15 +13,16 @@ print(f'Pssst, the solution is {chosen_word}')
 
 # Alternative approach
 
+word_length = len(chosen_word)
+
 display = []
-for _ in range(len(chosen_word)):
+for _ in range(word_length):
     display += "_"
 print(display)
 guess = input("Guess a letter: \n").lower()
 
-for position in range(len(chosen_word)):
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
 
-    if position == guess:
-        print("Right")
-    else:
-        print("Wrong")
