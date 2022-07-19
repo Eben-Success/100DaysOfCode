ffdab57092fd07a731f84987dc1561a212887e07
Eben-Success <aebenezer237@gmail.com>
2022-07-19 01:46:44 +0000

    update day 7

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -4,9 +4,6 @@ import random
 
 chosen_word = random.choice(word_list)
 
-for chosen_word in word_list:
-    print(chosen_word)
-
 guess = input("Guess a letter: \n").lower()
 
 for letter in chosen_word:
