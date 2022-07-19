1c5b042294092c6a5d188109ebc6b67e621aba62
Eben-Success <aebenezer237@gmail.com>
2022-07-19 10:20:03 +0000

    update hangman.py

--- Day 7/trial.py
+++ Day 7/trial.py
@@ -3,9 +3,13 @@ word_list = ["realmadrid", "barcelona", "chelsea", "arsenal"]
 
 chosen_word = random.choice(word_list)
 
+# Test the code by checking the random number
 print(f'\n Ops, the right word is {chosen_word} \n')
 
 word_length = len(chosen_word)
+
+# Display dashes equal to the random letter the user
+# has to guess
 
 display = []
 for _ in range(word_length):
