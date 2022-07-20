cebd78b7905fd24298b1e31fb3f0b9f480889187
Eben-Success <aebenezer237@gmail.com>
2022-07-20 10:24:08 +0000

    init PrimeNumberChecker.py

--- Day 8/PrimeNumberChecker.py
+++ Day 8/PrimeNumberChecker.py
@@ -1,6 +1,7 @@
 # Create a function that check whether or not an input number is a prime number.
 
 def prime_checker(number):
+
     for i in range(2, number):
         if number % i == 0:
             print("It is not a prime number")
@@ -8,4 +9,3 @@ def prime_checker(number):
             print("It is a prime number")
 
 
-pt
