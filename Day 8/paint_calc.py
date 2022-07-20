179a81c793b8a99d92b5442e2aef20be8f80da76
Eben-Success <aebenezer237@gmail.com>
2022-07-20 10:12:43 +0000

    done with paint_calc.py

--- Day 8/paint_calc.py
+++ Day 8/paint_calc.py
@@ -1,4 +1,12 @@
+import math
 
-def paint_calc():
+def paint_calc(height, width, cover):
     """The program calculates the number of cans needed to paint a wall given
     the height, width and coverage"""
+
+    area = height * width
+    num_of_cans = math.ceil(area/cover)
+
+    print(f"You'll need {num_of_cans} cans of paints")
+
+paint_calc(2,4,5)
