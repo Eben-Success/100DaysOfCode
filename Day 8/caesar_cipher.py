2bd5b0e016ac23e15d0a35944abc8ff3e453f93a
Eben-Success <aebenezer237@gmail.com>
2022-07-25 20:50:23 +0000

    start caesar-cipher

--- Day 8/caesar_cipher.py
+++ Day 8/caesar_cipher.py
@@ -1,5 +1,7 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
+direction = input("Type 'encode' to encrpyt, type 'decode' to decrypt:\n")
+text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number:\n"))
+
+ def encrypt(plain_text, shift_amount):
