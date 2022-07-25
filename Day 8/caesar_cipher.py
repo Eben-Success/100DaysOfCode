aeb833f16bfb6cdbd28cd0d9c8db3603684d568f
Eben-Success <aebenezer237@gmail.com>
2022-07-25 20:53:31 +0000

    start caesar-cipher

--- Day 8/caesar_cipher.py
+++ Day 8/caesar_cipher.py
@@ -4,4 +4,8 @@ direction = input("Type 'encode' to encrpyt, type 'decode' to decrypt:\n")
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number:\n"))
 
- def encrypt(plain_text, shift_amount):
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position =
