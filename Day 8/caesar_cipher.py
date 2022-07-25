148653e25fef07c68d56192144ab4c7f02fd5b42
Eben-Success <aebenezer237@gmail.com>
2022-07-25 20:57:47 +0000

    caesar_cipher.py

--- Day 8/caesar_cipher.py
+++ Day 8/caesar_cipher.py
@@ -8,4 +8,8 @@ def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
         position = alphabet.index(letter)
-        new_position =
+        new_position = position + shift_amount
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is {cipher_text}")
+
+def
