4eff65d1d3abcbb160bea65966059aa0343022c4
Eben-Success <aebenezer237@gmail.com>
2022-07-25 19:08:26 +0000

    revisit PyPassword Generator

---
+++ Day 5/Trial.py
@@ -0,0 +1,5 @@
+# Create a PyPassword Generator from scratch.
+
+import random
+
+letters = letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
