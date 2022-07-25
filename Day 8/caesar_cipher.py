682028bd0891e9a4b6ce9598c77109314625f4b2
Eben-Success <aebenezer237@gmail.com>
2022-07-25 21:00:09 +0000

    caesar_cipher.py

--- Day 8/caesar_cipher.py
+++ Day 8/caesar_cipher.py
@@ -12,4 +12,10 @@ def encrypt(plain_text, shift_amount):
         cipher_text += alphabet[new_position]
     print(f"The encoded text is {cipher_text}")
 
-def
+def decode(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        cipher_text = alphabet[new_position]
+    print(f"Your decoded text is {cipher_text}")
