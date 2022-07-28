4438a4bf15e64d9b61cad3b0a29210018bc4fa9f
Eben-Success <aebenezer237@gmail.com>
2022-07-28 09:06:20 +0000

    init challenge1

--- Day 9/trial.py
+++
@@ -1,6 +0,0 @@
-travel_log = {
-    "France":{"cities_visited":["Paris","Lille", "Dijon"], "total_visit": 12},
-    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart", "Stuttgart"], "total_visits":5}
-}
-
-print(travel_log)
