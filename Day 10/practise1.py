dc7ebd64e4d67b4f09a5a7b1c5c269ff85eed35b
Eben-Success <aebenezer237@gmail.com>
2022-07-29 10:50:23 +0000

    done with format_name

--- Day 10/practise1.py
+++ Day 10/practise1.py
@@ -1,13 +1,15 @@
-# Capitalize names
+# Funciton with output
 
-def format_name():
-    f_name = input("Enter yor first name\n").title()
-    l_name = input("Enter your last name:\n").title()
-
+def format_name(f_name, l_name):
     if f_name == "" or l_name == "":
-        return "You didn't provide valid inputs"
-    else:
-        print(f"{f_name}  {l_name}")
+        return "You didn't provide any valid inputs"
+
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
 
 
-format_name()
+f_name = input("What is your first name:\n")
+l_name = input("What is your last name:\n")
+
+print(format_name(f_name, l_name))
