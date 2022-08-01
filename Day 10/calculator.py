71cab49c04e9db79a83729bd7e7d45f637b7c231
Eben-Success <aebenezer237@gmail.com>
2022-08-01 00:55:21 +0000

    done with calculator.py

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -17,14 +17,26 @@ def multiply(n1, n2):
 
 # Divide
 def divide(n1, n2):
-    return n1 * n2
+    return n1 / n2
+
 
 operations = {
     "+": add,
     "-": subtract,
-    "*":multiply,
-    "/": divide
+    "*": multiply,
+    "/": divide,
 }
+
+
+num1 = int(input("What's the first number?\n"))
+num2 = int(input("What's the second number?\n"))
 
 for operation in operations:
     print(operation)
+
+chose_operation = input("Pick an operation from the line above\n")
+
+calculate_function = operations[chose_operation]
+answer = calculate_function(num1, num2)
+
+print(f"{num1} {chose_operation} {num2} = {answer}")
