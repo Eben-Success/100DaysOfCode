a0bc3718374b1e77405cdeec89005ecd36e487a9
Eben-Success <aebenezer237@gmail.com>
2022-08-01 00:43:59 +0000

    def operations

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -29,3 +29,12 @@ operations = {
 
 num1 = int(input("What's the first number?: \n"))
 num2 = int(input("What's the second number?: \n"))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above:\n")
+
+calculation_function = operations[operation_symbol]
+
+answer = calculation_function(num1, num2)
