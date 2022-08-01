e7fb30125b6b7bbadf961e7d0b714b519cffe4d2
Eben-Success <aebenezer237@gmail.com>
2022-08-01 12:38:18 +0000

    calculate 3 numbers

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -27,16 +27,29 @@ operations = {
     "/": divide,
 }
 
-
-num1 = int(input("What's the first number?\n"))
+num1 = int(input("What's the first number? \n"))
 num2 = int(input("What's the second number?\n"))
 
-for operation in operations:
-    print(operation)
 
-chose_operation = input("Pick an operation from the line above\n")
+def operation():
+    for symbol in operations:
+        print(symbol)
+
+
+operation()
+
+chose_operation = input("Pick an operation from the list above\n")
 
 calculate_function = operations[chose_operation]
+
 answer = calculate_function(num1, num2)
 
 print(f"{num1} {chose_operation} {num2} = {answer}")
+
+num3 = int(input("\nWhat's the third number"))
+operation()
+
+chose_operation = input("Pick an operation from the list above\n")
+final_answer = calculate_function(answer, num3)
+
+print(f"{answer} {chose_operation} {num3} = {final_answer}")
