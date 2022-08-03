5419a7e2e7968624498cd7796ff5a15c6aa349d2
Eben-Success <aebenezer237@gmail.com>
2022-08-03 10:43:37 +0000

    def calculator()

--- Day 10/calculator_refactored.py
+++ Day 10/calculator_refactored.py
@@ -27,15 +27,26 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What's the first number? \n"))
-for symbol in operations:
-    print(symbol)
 
-should_continue = True
+def calculator():
+    num1 = int(input("What's the first number? \n"))
+    for symbol in operations:
+        print(symbol)
 
-while should_continue:
-    symbol = input("Pick an operations:\n")
-    num2 = int(input("What's the next number:\n"))
-    calculation_function = operations[symbol]
-    first_answer = calculation_function(num1, num2)
-    print(f"{num1} {symbol} {num2} = {first_answer}")
+    should_continue = True
+
+    while should_continue:
+        symbol = input("Pick an operations:\n")
+        num2 = int(input("What's the next number:\n"))
+        calculation_function = operations[symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation:  ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
